6e4d9e2e5770247262d30669aadacc0aaf5593b4
Spix0r <spix0r@tuta.io>
2025-11-11 00:33:51 +0330

    Connect to the Database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,24 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export function ConnectToDatabase() {
+    mongoose.connection.on('open', () => {
+        console.info("Connected to Database.")
+    })
+    try {
+        const connection = mongoose.connect(process.env.MONGO)
+        return connection
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Backend is running on http://127.0.0.1:3000")
+    ConnectToDatabase()
 });
