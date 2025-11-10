4fb2e43249c5be99c764ef03e685077c4d95fdf7
Spix0r <spix0r@tuta.io>
2025-11-10 23:33:30 +0330

    Create the header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
+import Header from './components/Header'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/sign-in' element={<SignIn />}></Route>
         <Route path='/sign-up' element={<SignUp />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
-        <Route path='/projects' element={<Element />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { Button, Navbar, TextInput, NavbarToggle, NavbarCollapse, NavbarLink  } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon } from 'react-icons/fa'
+export default function Header() {
+    const path = useLocation.path;
+  return (
+    <Navbar className='border-b-2'>
+        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <span className='px-1'>Safe</span>
+        Blog
+        </Link>
+        <form>
+            <TextInput
+            type='text'
+            placeholder='Search...'
+            rightIcon={AiOutlineSearch}
+            className='hidden lg:inline'
+            color='gray'
+            >
+            </TextInput>
+        </form>
+        <Button className='lg:hidden' color='gray' pill>
+            <AiOutlineSearch/>
+        </Button>
+        <div className="flex gap-2 md:order-2">
+            <Button className='hidden sm:inline' color='gray' pill>
+                <FaMoon/>
+            </Button>
+            <Link to='/sign-in'>
+            <Button color='gray'>
+            Sign In
+            </Button>
+            </Link>
+            <NavbarToggle/>
+        </div>
+        <NavbarCollapse>
+                <NavbarLink active={path === "/" } as={'div'}>
+                    <Link to="/">
+                    Home
+                    </Link>
+                </NavbarLink>
+                <NavbarLink active={path === "/about"} as={'div'}>
+                    <Link to="/about">
+                    About
+                    </Link>
+                </NavbarLink>
+                <NavbarLink active={path === "/projects"} as={'div'}>
+                    <Link to="/projects">
+                    Projects
+                    </Link>
+                </NavbarLink>
+            </NavbarCollapse>
+    </Navbar>
+  )
+}
