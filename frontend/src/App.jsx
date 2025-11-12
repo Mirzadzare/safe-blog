b422e1810bfbc38dae5fa5e6c80c1f66e4e8876b
Spix0r <spix0r@tuta.io>
2025-11-13 00:28:34 +0330

    Make Dashboard Private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Projects from './pages/Projects'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -20,7 +21,9 @@ export default function App() {
             <Route path='/about' element={<About />} />
             <Route path='/sign-in' element={<SignIn />} />
             <Route path='/sign-up' element={<SignUp />} />
+            <Route element={<PrivateRoute />}>
             <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
             <Route path='/projects' element={<Projects />} />
           </Routes>
         </main>
